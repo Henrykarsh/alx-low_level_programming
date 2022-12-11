e08bf9e3df0950b42d3c17c3d0f7220fb28ef739
Henrykarsh <henrykahoro@outlook.com>
2022-12-11 12:25:24 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
@@ -12,5 +12,6 @@ int main(void)
 {
 	write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+
 
 }
